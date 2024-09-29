decfe3df8356735647534815eb3cb087821858f9
mfg637 <mfg636@gmail.com>
2024-09-29 18:56:28 +0300

    add cosine sheduler

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from .guiders import SheduledGuider
+from .shedulers import cosine_scheduler
 
 NODE_CLASS_MAPPINGS = {
-    "SheduledCFGGuider": SheduledGuider.SheduledCFGGuider
+    "SheduledCFGGuider": SheduledGuider.SheduledCFGGuider,
+    "CosineScheduler": cosine_scheduler.CosineScheduler,
 }
 __all__ = ['NODE_CLASS_MAPPINGS']
---
+++ shedulers/cosine_scheduler.py
@@ -0,0 +1,32 @@
+import math
+
+import numpy
+import torch
+
+
+class CosineScheduler:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                {"steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                 "sigma_max": ("FLOAT", {"default": 1, "min": 0.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "sigma_min": ("FLOAT", {"default": 0, "min": 0.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "period": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 }
+                }
+    RETURN_TYPES = ("SIGMAS",)
+    CATEGORY = "sampling/custom_sampling/schedulers"
+
+    FUNCTION = "get_sigmas"
+
+    def get_sigmas(self, steps, sigma_max, sigma_min, period):
+        sigmas = numpy.zeros(steps)
+        delta_s = 1 / steps
+        minmax_delta = sigma_max - sigma_min
+        period_value = math.pi * period * 2
+
+        for step in range(steps):
+            sigmas[step] = ((math.cos(step * delta_s * period_value) / 2 + 0.5)
+                            * minmax_delta + sigma_min)
+
+        return (torch.FloatTensor(sigmas), )
