12e0f6a9d75ea0aa2cfbc3f3255bdfb9992ed50d
mfg637 <mfg636@gmail.com>
2024-10-27 11:14:40 +0200

    bugfix

--- shedulers/cosine_scheduler.py
+++ shedulers/cosine_scheduler.py
@@ -24,9 +24,12 @@ class CosineScheduler:
         delta_s = 1 / steps
         minmax_delta = sigma_max - sigma_min
         period_value = math.pi * period * 2
+        period_min = math.cos(min(math.pi, period_value))
+        divider = 1 - period_min
 
         for step in range(steps):
-            sigmas[step] = ((math.cos(step * delta_s * period_value) / 2 + 0.5)
-                            * minmax_delta + sigma_min)
+            sigmas[step] = ((math.cos(step * delta_s * period_value)
+                             - period_min)
+                            / divider * minmax_delta + sigma_min)
 
         return (torch.FloatTensor(sigmas), )
