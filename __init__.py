e514120f1c4d33542d0630870a8209d859f9e16c
mfg637 <mfg636@gmail.com>
2024-11-28 21:11:36 +0200

    add gausian scheduler

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 from .guiders import SheduledGuider
-from .shedulers import cosine_scheduler
+from .shedulers import cosine_scheduler, gaussian
 from .sigmas import add_zsnr_sigma, concat_sigmas, invert_sigmas, offset_sigmas
 
 NODE_CLASS_MAPPINGS = {
     "ScheduledCFGGuider": SheduledGuider.SheduledCFGGuider,
     "PerpNegScheduledCFGGuider": SheduledGuider.PerpNegSheduledCFGGuider,
     "CosineScheduler": cosine_scheduler.CosineScheduler,
+    "GaussianScheduller": gaussian.GaussianScheduler,
     "Add zSNR Sigma max": add_zsnr_sigma.AddZsnrSigma,
     "InvertSigmas": invert_sigmas.InvertSigmas,
     "ConcatSigmas": concat_sigmas.ConcatSigmas,
---
+++ shedulers/gaussian.py
@@ -0,0 +1,60 @@
+import math
+
+import numpy
+import torch
+
+
+class GaussianScheduler:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                {"steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                 "sigma_max": ("FLOAT", {"default": 1, "min": 0.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "sigma_min": ("FLOAT", {"default": 0, "min": 0.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "mean": ("FLOAT", {"default": 0.0, "min": -5000.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "standard_deviation": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "a": ("FLOAT", {"default": 0.0, "min": -5000.0, "max": 5000.0, "step": 0.01, "round": False}),
+                 "b": ("FLOAT", {"default": 1.0, "min": -5000.0, "max": 5000.0, "step": 0.01, "round": False})
+                 }
+                }
+    RETURN_TYPES = ("SIGMAS",)
+    CATEGORY = "sampling/custom_sampling/CFG-schedulers"
+
+    FUNCTION = "get_sigmas"
+
+    def get_sigmas(
+            self,
+            steps: int,
+            sigma_max: float,
+            sigma_min: float,
+            mean: float,
+            standard_deviation: float,
+            a: float,
+            b: float
+            ):
+        stdev = standard_deviation
+        if a > b:
+            _a = a
+            _b = b
+            a = _b
+            b = _a
+        elif a == b:
+            raise ValueError("Value A must not be equal to B")
+        sigmas = numpy.zeros(steps)
+        delta_s = (b - a) / steps
+        minmax_delta = sigma_max - sigma_min
+
+        constant_left_part = 1/(stdev * math.sqrt(2 * math.pi))
+        for step in range(steps):
+            x = step * delta_s + a
+            right_part = math.exp(-1 * (x - mean)**2/(stdev**2*2))
+            sigmas[step] = constant_left_part * right_part
+
+        min_value = min(sigmas)
+        max_value = max(sigmas)
+        value_minmax_delta = max_value - min_value
+        scale_coef = minmax_delta / value_minmax_delta
+        for step in range(steps):
+            sigmas[step] = (sigmas[step] - min_value) * scale_coef + sigma_min
+
+        return (torch.FloatTensor(sigmas), )
